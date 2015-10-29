01a78ee1d576bb1ee7ebe8394771c9db4507860a
kuramameng <mengjs324@gmail.com>
2015-10-29 13:24:15 -0400

    response

--- script/update_people.rb
+++ script/update_people.rb
@@ -2,9 +2,9 @@ Licensee.transaction do
   licensees = Licensee.all
   licensees.each do |licensee|
 
-    if (licensee.surname = 'Escobar' && licensee.given_name = 'Marilynn')
+    if (licensee.surname == 'Escobar' && licensee.given_name == 'Marilynn')
       licensee.height -= 1
-    elsif (licensee.surname = 'Whaley' && licensee.given_name = 'Chris')
+    elsif (licensee.surname == 'Whaley' && licensee.given_name == 'Chris')
       licensee.height -= 1
     end
     licensee.save!
