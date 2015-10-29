a2569ce902a1d0ef53965c8eeedbc63f89ddbc14
EdieShimel <edieshimel@gmail.com>
2015-10-29 12:04:10 -0400

    oops! forgot this! assessments.rb

--- assessment.rb
+++ assessment.rb
@@ -2,6 +2,8 @@ require 'csv'
 # Assessment answers to:
 
 # question 2:
+licensee.create(surname: 'Bishop', given_name: 'Caren', height:63, weight: 132, dob: '1943-09-26', gender: 'f') ? puts('success') : puts('fail')
+p person
 
 # question 3:
 
