80c1905cfbb2e5315d3b2087175a7b4f7460e7c5
Holden Mills <mholdenmills@gmail.com>
2015-10-29 12:03:01 -0400

    Answered q's

--- assessment.rb
+++ assessment.rb
@@ -1,8 +1,14 @@
 require 'csv'
 # Assessment answers to:
+Generate a model (`Licensee`) and migration (`db/migrate/<time stamp>_create_licensees.rb`) to hold the data in `data/licensees.csv`.  Run `rake db:migrate` to create the table.
 
+class Licensee < ActiveRecord::Migration
+  def create_table "licensees"
+    surname: CHARACTER VARYING
+    given name: CHARACTER VARYING
+  end
 # question 2:
-
+licensee = Licensee.create
 # question 3:
 
 # question 4:
