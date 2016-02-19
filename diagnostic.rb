fc28a82ee7af823a1ecb6e1c39cb275bab83303a
Celena T <chibiynm@gmail.com>
2016-02-19 09:32:09 -0500

    response

--- diagnostic.rb
+++ diagnostic.rb
@@ -1,7 +1,9 @@
 require 'csv'
 # Diagnostic answers to:
-
+# rails g model Licensee surname:string given_name:string gender:string height:integer weight:integer born_on:date eye_color:string hair_color:string
 # question 2:
+
+Licensee.new('Bishop','Caren','f',63,132,'1943-09-26','Brown','Black')
 
 # question 3:
 Licensee.transaction do
@@ -10,7 +12,12 @@ Licensee.transaction do
 end
 
 # question 4:
+Licensee.where hair_color: %w(Hazel), eye_color: %(Black Brown)
 
 # question 5:
+Licensee.where surname: %w(Escobar Whaley) given_name: %w(Marilynn Chris)
+rails g migration ChangeHeightToOneInchShorter height:height-1
 
 # question 6:
+RichAndTeresita = Licensee.where surname: %w(Rich Myers) given_name: %w(Dylan Teresita)
+RichAndTeresita.destroy
