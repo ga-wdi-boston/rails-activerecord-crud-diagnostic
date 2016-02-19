d34e3cf58011180320bf907e37d5e7c3c67cb5da
Ethan <erdesi90562@gmail.com>
2016-02-19 09:30:22 -0500

    Was woking on mostly Q2 and Q3. Never made it past them.

---
+++ scripts/create_person.rb
@@ -0,0 +1,8 @@
+caren = License.create surname: 'Bishop',
+                      given_name: 'Caren',
+                      gender: 'f',
+                      height: 63,
+                      weight: 132,
+                      born_on: '1943-09-26',
+                      eye_color: 'Brown',
+                      hair_color: 'Black'
