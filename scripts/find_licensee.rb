96a0c62642d788848215fb85246c7079bc8da0a2
byau <billy.l.yau@gmail.com>
2016-02-19 09:31:09 -0500

    response

---
+++ scripts/find_licensee.rb
@@ -0,0 +1,5 @@
+licensees = Licensee.where ('eye_color = :eye_color' AND 'hair_color = :hair_color'),
+                  eye_color: 'Hazel'
+                  hair_color: 'Brown', 'Black'
+
+p licensees
