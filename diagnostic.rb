7ef039de2fb8fb4f24b30a47d8fa85e20f9cf8b5
Jeremiah <jeremiahdamiron@gmail.com>
2016-02-19 09:26:39 -0500

    response

--- diagnostic.rb
+++ diagnostic.rb
@@ -2,10 +2,20 @@ require 'csv'
 # Diagnostic answers to:
 
 # question 2:
+Licensee.create surname: 'Bishop',
+given_name: 'Caren',
+gender: 'f',
+height: 63,
+weight: 132,
+born_on: '1943-09-26',
+eye_color: 'Brown',
+hair_color: 'Black'
 
 # question 3:
 Licensee.transaction do
   CSV.foreach 'data/licensees.csv', headers: true do |row|
+    licensee = row.to_hash
+    Licensee.create! licensee unless Licensee.exists? licensee
   end
 end
 
