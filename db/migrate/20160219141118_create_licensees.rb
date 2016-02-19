2a685c1dd9e18e3cfd51a9843d5ce6e9923ffa8e
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-02-19 09:30:38 -0500

    nw resp ran out of time

---
+++ db/migrate/20160219141118_create_licensees.rb
@@ -0,0 +1,18 @@
+class CreateLicensees < ActiveRecord::Migration
+  def change
+    create_table :licensees do |t|
+      t.string :surname
+      t.string :given_name
+      t.string :gender
+      t.string :string
+      t.string :height
+      t.string :integer
+      t.integer :weight
+      t.date :born_on
+      t.string :eye_color
+      t.string :hair_color
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ scripts/add_licensee.rb
@@ -0,0 +1,10 @@
+lic = Licensee.new surname: 'Bishop',
+                      given_name: 'Caren',
+                      gender:'f',
+                      height: 63,
+                      weight: 132,
+                      born_on: '1943-09-26',
+                      eye_color: 'Brown',
+                      hair_color: 'Black'
+lic.save!
+p lic
