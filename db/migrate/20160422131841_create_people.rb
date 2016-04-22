e6fd3705d2e1c7793c1ede172b6e4147f88852c5
sjf125 <sjf125@gmail.com>
2016-04-22 09:28:17 -0400

    Cant open Atom

---
+++ db/migrate/20160422131841_create_people.rb
@@ -0,0 +1,16 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :surname
+      t.string :given_name
+      t.string :gender
+      t.integer :height
+      t.integer :weight
+      t.date :born_on
+      t.string :eye_color
+      t.string :hair_color
+
+      t.timestamps null: false
+    end
+  end
+end
