1a34db10276b6de7696ff1eccbdd6a33216e7a51
mattb5 <mattbuffardi@gmail.com>
2016-06-17 09:40:11 -0400

    diagnostic submission

--- diagnostic.rb
+++ diagnostic.rb
@@ -2,6 +2,10 @@ require 'csv'
 # Diagnostic answers to:
 
 # question 2:
+Licensee.create([
+  { title: 'Hello', artist: 'Hello', year: 1998 },
+  Bishop, Caren, f, 63, 132, 1943-09-26, Brown, Black
+])
 
 # question 3:
 Licensee.transaction do
