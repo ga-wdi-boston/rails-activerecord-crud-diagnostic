fc505271ae94b91f0312bc333dea5c7f2d49bd52
MishaHerscu <mishaherscu@gmail.com>
2016-06-17 09:54:48 -0400

    respaced things for linter

--- diagnostic.rb
+++ diagnostic.rb
@@ -5,6 +5,17 @@ require 'csv'
 Licensee.CreateLicensees('Bishop', 'Caren', 'f', 63, 132, '1943-09-26', 'Brown',
                          'Black')
 
+Licensee.create(
+  surname: 'Bishop',
+  given_name: 'Caren',
+  gender: 'f',
+  height: 63,
+  weight: 132,
+  born_on: '1943-09-26',
+  eye_color: 'Brown',
+  hair_color: 'Black'
+)
+
 # question 3:
 Licensee.transaction do
   CSV.foreach 'data/licensees.csv', headers: true do |row|
@@ -13,10 +24,10 @@ Licensee.transaction do
 end
 
 # question 4:
-licensees = Licensee.where(eye_color: 'Hazel', hair_color: 'brown' ||
-hair_color: 'black')
+Licensee.where(eye_color: 'Hazel').where(hair_color: 'brown').or(hair_color:
+'black')
 
 # question 5:
-licensee = Licensee.where(surname: 'Rich', given_name: 'Dylan' ||
-                          surname: 'Myers', given_name: 'Teresita')
+licensee = Licensee.where(surname: 'Rich', given_name: 'Dylan').or(surname:
+'Myers', given_name: 'Teresita')
 licensee.destroy
