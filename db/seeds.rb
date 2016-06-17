ccc6b4ee7c7c3e7dd2bdcfc38f437ea08d50b34f
erinsul <erin_sullivan50@yahoo.com>
2016-06-17 09:36:24 -0400

    Still trying
    The timing confused me.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Licensee.transaction do
+  CSV.foreach 'data/licensees.csv', headers: true do |row|
