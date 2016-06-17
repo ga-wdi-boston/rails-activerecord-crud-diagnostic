8002ad295af71d3218173ded3b76b45874e36176
RoobScoobs <iruben229@gmail.com>
2016-06-17 09:26:14 -0400

    Rails activerecord crud diagnostic

---
+++ app/controllers/licensees_controller.rb
@@ -0,0 +1,59 @@
+class LicenseesController < ApplicationController
+  before_action :set_licensee, only: [:show, :update, :destroy]
+
+  # GET /licensees
+  # GET /licensees.json
+  def index
+    @licensees = Licensee.all
+
+    render json: @licensees
+  end
+
+  # GET /licensees/1
+  # GET /licensees/1.json
+  def show
+    render json: @licensee
+  end
+
+  # POST /licensees
+  # POST /licensees.json
+  def create
+    @licensee = Licensee.new(licensee_params)
+
+    if @licensee.save
+      render json: @licensee, status: :created, location: @licensee
+    else
+      render json: @licensee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /licensees/1
+  # PATCH/PUT /licensees/1.json
+  def update
+    @licensee = Licensee.find(params[:id])
+
+    if @licensee.update(licensee_params)
+      head :no_content
+    else
+      render json: @licensee.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /licensees/1
+  # DELETE /licensees/1.json
+  def destroy
+    @licensee.destroy
+
+    head :no_content
+  end
+
+  private
+
+    def set_licensee
+      @licensee = Licensee.find(params[:id])
+    end
+
+    def licensee_params
+      params[:licensee]
+    end
+end
---
+++ app/models/licensee.rb
@@ -0,0 +1,3 @@
+class Licensee < ActiveRecord::Base
+  new_licensee = Licensee.create(surname: "Bishop", given_name: "Caren", gender: "f", height: 63, weight: 132, born_on: '1943-09-26', eye_color: "Brown", hair_color: "Black")
+end
