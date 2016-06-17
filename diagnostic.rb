fb54b399ebd282d4e1bcced728a5247acd425585
erinsul <erin_sullivan50@yahoo.com>
2016-06-17 09:27:52 -0400

    Added some answers and code...
    I tried but there's no time left.

--- diagnostic.rb
+++ diagnostic.rb
@@ -2,7 +2,10 @@ require 'csv'
 # Diagnostic answers to:
 
 # question 2:
-
+# rails-api generate migration \
+# AddSurnameGivennameGenderHeightWeightBornonEyecolorHairColorToLicensees \
+# surname:string given_name:string gender:string height:integer weight:integer \
+# born_on:string eye_color:string hair_color:string
 # question 3:
 Licensee.transaction do
   CSV.foreach 'data/licensees.csv', headers: true do |row|
@@ -10,5 +13,22 @@ Licensee.transaction do
 end
 
 # question 4:
-
-# question 5:
+# added to LicenseesController licensee_params
+# params.require(:licensee).permit(:surname, :given_name, :gender, :born_on, :height, :weight, :eye_color, :hair_color)
+#
+# curl --include --request PATCH http://localhost:3000/licensees/8 \
+#    --header "Content-Type: application/json" \
+#    --data '{
+#        "licensee": {
+#             "surname": "Hilo",
+#             "given_name": 330,
+#             "gender": 4
+#             "born_on":
+#             "height":
+#             "weight":
+#             "eye_color":
+#             "hair_color":
+#        }
+#    }'
+# # question 5:
+# curl --include --request DELETE http://localhost:3000/licensees/ID
